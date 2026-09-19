47feeca62dd63be9e58d790aded62a28cac529a4
Dmytro Berehovyi <nusdrel@gmail.com>
2026-09-20 00:59:53 +0300

    Implement student A algorithm

--- main.cpp
+++ main.cpp
@@ -16,5 +16,17 @@ int main() {
     });
 
     std::cout << "Project Initialized!" << std::endl;
+    
+    // Виклик алгоритму Студента А
+    auto resultA = calculateA(data);
+    
+    // Використання structured bindings для отримання результату
+    auto [extremum_x_A, extremum_y_A, iterations_A] = *resultA; 
+    
+    std::cout << "\n--- Result of Student A (Golden Section) ---" << std::endl;
+    std::cout << "Extremum X: " << extremum_x_A << std::endl;
+    std::cout << "Extremum Y: " << extremum_y_A << std::endl;
+    std::cout << "Iterations: " << iterations_A << std::endl;
+
     return 0;
 }
--- student_a.cpp
+++ student_a.cpp
@@ -1,6 +1,45 @@
 #include "shared_types.h"
 #include <memory>
+#include <cmath>
 
 std::unique_ptr<Result> calculateA(std::shared_ptr<const InputData> data) {
-    return std::make_unique<Result>(Result{0.0, 0.0, 0});
+    double a = data->a;
+    double b = data->b;
+    double epsilon = data->epsilon;
+    
+    // Пропорція золотого перерізу
+    const double phi = (std::sqrt(5.0) - 1.0) / 2.0;
+    
+    double x1 = b - phi * (b - a);
+    double x2 = a + phi * (b - a);
+    
+    double y1 = data->f(x1);
+    double y2 = data->f(x2);
+    
+    int iterations = 0;
+    
+    // Шукаємо мінімум функції
+    while (std::abs(b - a) > epsilon) {
+        if (y1 < y2) {
+            b = x2;
+            x2 = x1;
+            y2 = y1;
+            x1 = b - phi * (b - a);
+            y1 = data->f(x1);
+        } else {
+            a = x1;
+            x1 = x2;
+            y1 = y2;
+            x2 = a + phi * (b - a);
+            y2 = data->f(x2);
+        }
+        iterations++;
+    }
+    
+    // Знаходимо координати точки мінімуму
+    double x_min = (a + b) / 2.0;
+    double y_min = data->f(x_min);
+    
+    // Повертаємо результат через std::unique_ptr
+    return std::make_unique<Result>(Result{x_min, y_min, iterations});
 }
